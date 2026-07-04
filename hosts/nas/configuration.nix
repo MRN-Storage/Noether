fc60452aac16f3e138909289e5cf62941b7e3c82
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-04 20:21:00 +0200

    only fall back

--- hosts/nas/configuration.nix
+++ hosts/nas/configuration.nix
@@ -46,7 +46,7 @@ boot.initrd.luks.devices."cryptdata" = {
     # USB stick keyfile — the raw USB device/partition is used directly as the key.
     # keyFile = "/dev/disk/by-id/usb-USB_Flash_Disk_SCY0000000039178-0:0";
     # keyFileSize = 4096;
-    # fallbackToPassword = true;
+    fallbackToPassword = true;
   };
 
 
