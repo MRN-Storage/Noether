d1e70fe2f6939880c4df32985c85c43e2e9519a6
Christian Reck (DjPandup) <djpandup@gmail.com>
2026-07-06 20:02:20 +0200

    fix proxy adress

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -1,15 +1,12 @@
 { ... }:
 
 {
-  networking.hosts = {
-    "10.100.0.2" = [ "auth.nas.lan" ];
-  };
 
   services.caddy = {
     enable = true;
     virtualHosts."auth.nas.lan".extraConfig = ''
       tls internal
-      reverse_proxy auth.nas.lan {
+      reverse_proxy https://10.100.0.2:8443 {
         transport http {
           tls
           tls_insecure_skip_verify
