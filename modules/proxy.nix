82a37a18c4fb89cb9fed17ef9189e614f687ce28
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-06 19:17:57 +0200

    replaced nginx with caddy

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -1,23 +1,15 @@
 { ... }:
 
 {
-  services.nginx = {
+  services.caddy = {
     enable = true;
-    recommendedProxySettings = true;
-    recommendedTlsSettings = true;
-
-    virtualHosts."photos.nas.lan" = {
-      # TODO: enableACME + forceSSL once you have a real domain/DNS pointed here,
-      # or use Tailscale + self-signed certs for LAN-only access.
-      locations."/".proxyPass = "http://127.0.0.1:2283";
-    };
-
-    virtualHosts."files.nas.lan" = {
-      locations."/".proxyPass = "http://127.0.0.1:3923";
-    };
-
-    virtualHosts."auth.nas.lan" = {
-      locations."/".proxyPass = "https://10.100.0.2:8443";
-    };
+    virtualHosts."auth.nas.lan".extraConfig = ''
+      reverse_proxy http://10.100.0.2:8443 {
+        transport http {
+          tls
+          tls_insecure_skip_verify
+        }
+      }
+    '';
   };
 }
