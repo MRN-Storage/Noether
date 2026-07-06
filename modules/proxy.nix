ac11252b3fe162fc3c16d10479d10ba24fc4d692
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-06 19:33:54 +0200

    fix fix

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -4,8 +4,8 @@
   services.caddy = {
     enable = true;
     virtualHosts."auth.nas.lan".extraConfig = ''
+      tls internal
       reverse_proxy 10.100.0.2:8443 {
-        tls internal
         transport http {
           tls
           tls_insecure_skip_verify
