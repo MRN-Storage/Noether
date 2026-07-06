025ff1d44d623fa3ae18e34f53d4c21a55b7490f
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-06 19:30:59 +0200

    fix

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -5,6 +5,7 @@
     enable = true;
     virtualHosts."auth.nas.lan".extraConfig = ''
       reverse_proxy 10.100.0.2:8443 {
+        tls internal
         transport http {
           tls
           tls_insecure_skip_verify
