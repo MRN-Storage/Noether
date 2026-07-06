37fdaaf2f4eb3a010de25b2816405a9eed20afec
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-06 20:37:58 +0200

    extract secrets

--- modules/kanidm-provision.nix
+++ modules/kanidm-provision.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   services.kanidm.provision = {
+    services.kanidm.provision.adminPasswordFile = "/run/secrets/kanidm-admin-password"
     persons.user = {
       legalName = "User User";
     };
