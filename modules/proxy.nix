0e15057430cadb8ba616d1fa3d483520a804b1fd
Ell Ari Nitsche <mail@ellanitsche.de>
2026-07-06 19:41:09 +0200

    i am not so fast

--- modules/proxy.nix
+++ modules/proxy.nix
@@ -4,12 +4,12 @@
   networking.hosts = {
     "10.100.0.2" = [ "auth.nas.lan" ];
   };
-  
+
   services.caddy = {
     enable = true;
     virtualHosts."auth.nas.lan".extraConfig = ''
       tls internal
-      reverse_proxy 10.100.0.2:8443 {
+      reverse_proxy auth.nas.lan {
         transport http {
           tls
           tls_insecure_skip_verify
