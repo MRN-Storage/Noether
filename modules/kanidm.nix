da0473bd7144c5c5ff6f1dfb803971f5692ab25f
Christian Reck <chrreck19@web.de>
2026-07-06 19:50:52 +0200

    Remove internal TLS settings from Kanidm configuration
    Removed internal TLS configuration for Kanidm server settings.

--- modules/kanidm.nix
+++ modules/kanidm.nix
@@ -8,10 +8,6 @@
       domain = "auth.nas.lan";
       origin = "https://auth.nas.lan";
       bindaddress = "0.0.0.0:8443";
-      
-      # instruct Kanidm to handle its own internal TLS generation:
-      tls_chain = null;
-      tls_key = null;
     };
   };
 
