08484e5ead4dbf94ddc1260c3a14f57850ce4b08
Christian Reck (DjPandup) <djpandup@gmail.com>
2026-07-07 19:40:36 +0200

    Revert "replace uri with internal ip"
    This reverts commit 34e2e51ccef3f07414f92b8c17c75ae5bdfacc58.

--- vms/kanidm/default.nix
+++ vms/kanidm/default.nix
@@ -23,7 +23,7 @@
 
   services.kanidm.enableClient = true;
   
-  services.kanidm.client.settings.uri = "10.100.0.2";
+  services.kanidm.client.settings.uri = "https://auth.nas.lan";
 
   networking.hostName = "kanidm-vm";
   networking.useNetworkd = true;
