5b0197afabc182389c9c9a89edc7d60e287ad8e1
Christian Reck (DjPandup) <djpandup@gmail.com>
2026-07-07 20:19:01 +0200

    add OIDC to immich config

--- modules/immich.nix
+++ modules/immich.nix
@@ -5,7 +5,16 @@
     enable = true;
     port = 2283;
     mediaLocation = "/data/immich";
-    # Configure OIDC once Kanidm is up (see modules/kanidm.nix for the OAuth2 client setup):
-    # environment = { OIDC_ISSUER_URL = "https://auth.nas.lan/oauth2/openid/immich"; };
+
+    environment = {
+    IMMICH_OIDC_ENABLED = "true";
+
+    IMMICH_OIDC_ISSUER_URL =
+      "https://auth.nas.lan/oauth2/openid/immich";
+
+    IMMICH_OIDC_CLIENT_ID = "immich";
+
+    IMMICH_OIDC_CLIENT_SECRET =
+      "exz7gt9h4dbbkjhbxvzd4wpqb9ut3fdxrkucrzw3y1w4zpfd";
   };
 }
